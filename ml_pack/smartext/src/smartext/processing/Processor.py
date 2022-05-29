a0f5d3f4443dd7ecb5d7529e972c295f2a1f7a4a
Christos Constantinou <aq21501@bc4gpulogin1.bc4.acrc.priv>
2022-05-29 17:17:03 +0100

    create vocab method

--- ml_pack/smartext/src/smartext/processing/Processor.py
+++ ml_pack/smartext/src/smartext/processing/Processor.py
@@ -15,4 +15,38 @@ class SimpleProcessor(ProcessorMain):
         self.documents = documents
 
 
+    def create_vocab(self,documents:List,char_to_split:str=' ',return_pd:bool=True) -> None:
+
+        self.vocab = {}
+
+        # loop documents
+        for doc in documents:
+
+            for word in self.split_simple(doc,char_to_split):
+
+                try:
+                    vocab[word] += 1
+                except:
+                    vocab[word] = 1
+        
+        if return_pd:
+            df_vocab = pd.DataFrame({
+                'word': list(vocab.keys()),
+                'freq': list(vocab.values())
+            }
+            ).sort_values(by=['freq'],ascending=False)
+
+            self.vocab = df_vocab
+
+        return self.vocab
+
+
+    def split_simple(self,doc:str,char_to_split:str=' ') -> List:
+
+        return doc.split(char_to_split)
+
+
+
+
+
 
